68365bcc6278f1cb30d76162da877d5cfd416929
dada0524 <133331237+dada0524@users.noreply.github.com>
2024-07-16 09:54:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,19 +7,6 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 let addresses = [
 	'icook.tw:2053#官方优选域名',
 	'cloudflare.cfgo.cc#优选官方线路',
-];
-
-// 设置优选地址api接口
-let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
-];
-
-// 设置优选地址，不带端口号默认80，noTLS订阅生成
-let addressesnotls = [
-	'www.visa.com.sg#官方优选域名',
-	'www.wto.org:8080#官方优选域名',
-	'www.who.int:8880#官方优选域名',
 	'bestcf.onecf.eu.org#官方优选域名-Mingyu大佬提供维护',
         'bestproxy.onecf.eu.org#反代优选域名-Mingyu大佬提供维护',
         'cfip.xxxxxxxx.tk#官方优选域名-OTC大佬提供维护',
@@ -71,6 +58,20 @@ let addressesnotls = [
         'www.iakeys.com',
         'www.d-555.com',
         'fbi.gov',
+];
+
+// 设置优选地址api接口
+let addressesapi = [
+	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+];
+
+// 设置优选地址，不带端口号默认80，noTLS订阅生成
+let addressesnotls = [
+	'www.visa.com.sg#官方优选域名',
+	'www.wto.org:8080#官方优选域名',
+	'www.who.int:8880#官方优选域名',
+	
 	
 ];
 
